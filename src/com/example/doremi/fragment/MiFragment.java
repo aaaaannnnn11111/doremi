b73a6f232589396af3a28a385de3ff37ecaa8842
jingzjj <295868420@qq.com>
2015-10-29 12:47:09 +0800

    Mi界面完善

--- src/com/example/doremi/fragment/MiFragment.java
+++ src/com/example/doremi/fragment/MiFragment.java
@@ -1,27 +1,44 @@
 package com.example.doremi.fragment;
 
 
+import com.example.doremi.MoreActivity;
 import com.example.doremi.R;
-import com.example.doremi.R.layout;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.ScrollView;
 
 public class MiFragment extends Fragment {
 	private ScrollView scroll;
+	private Button mimore;
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,  
             Bundle savedInstanceState) {  
         View miLayout = inflater.inflate(R.layout.mi_layout, container, false); 
         scroll  = (ScrollView) miLayout.findViewById(R.id.miScrollView);
+        mimore  = (Button) miLayout.findViewById(R.id.mimore);
+        mimore.setOnClickListener(new NewButtonListener());
         return miLayout;  
     }  
+	
+	 class NewButtonListener implements OnClickListener{  
+		   
+	        @Override  
+	        public void onClick(View arg0) {  
+	            // TODO Auto-generated method stub  
+	        	Intent intent = new Intent();  
+	            intent.setClass(getActivity(), MoreActivity.class);  
+	            getActivity().startActivity(intent); 
+	        }  
+	          
+	    } 
 	
 
 }
